23cd748102e0fc5000e384b87bdb1971be970cd7
chian-chen <64936325+chian-chen@users.noreply.github.com>
2022-07-04 17:19:44 +0800

    add color transition and Rotation and Score system, BUT score system is stupid git add .

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,50 @@
-startTime = 11 / 30; bpm = 146 / 4; playbackSpeed = 1;
-interval = 60000 / bpm / playbackSpeed;
-bgm = document.getElementById("bgm");
-dot = document.getElementById("dotGray");
-bgm.currentTime = startTime; bgm.playbackRate = playbackSpeed;
+const startTime = 11 / 30;
+const bpm = 146 / 4;
+const playbackSpeed = 1;
+
+const interval = 60000 / bpm / playbackSpeed;
+
+const bgm = document.getElementById("bgm");
+const dot = document.getElementById("dotGray");
+const score = document.getElementById("score");
+
+bgm.currentTime = startTime;
+bgm.playbackRate = playbackSpeed;
 bgm.play();
-intervalID = setInterval(getRandomXY, interval);
+
+const intervalID = setInterval(getRandomXY, interval);
+
+const newspaperSpinning = [
+  { transform: ' scale(1)' },
+  { transform: ' scale(0)' }
+];
+
+const newspaperTiming = {
+  duration: interval + 500,
+  iterations: 1,
+}
 
 function getRandomXY() {
     dot.innerHTML = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-    dot.style.top = (Math.random() * 85) + "vh";
-    dot.style.left = (Math.random() * 85) + "vw";
+
+    dot.style.top = (15 + Math.random() * 70) + "vh";
+    dot.style.left = (15 + Math.random() * 70) + "vw";
+    dot.animate(newspaperSpinning, newspaperTiming);
     dot.style.backgroundColor = "Azure";
 }
 
+let count = 0;
+
 document.addEventListener("keydown", (event) => {
-    if (event.key == dot.innerHTML.toLowerCase()) {
+    if (event.key == dot.innerHTML.toLowerCase()){
         dot.style.backgroundColor = "Aquamarine";
+        count++;
+        score.innerHTML = "Score: "+count;
     }
-    else {
+    else{
         dot.style.backgroundColor = "Coral";
+        count--;
+        score.innerHTML = "Score: "+count;
+
     }
 })
